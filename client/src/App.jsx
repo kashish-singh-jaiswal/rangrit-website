d3e48f8e309587d715a79b77289f5e33ce3f74dc
Kashish Singh Jaiswal <kashishsinghjaiswal4@gmail.com>
2026-07-11 18:44:27 +0530

    Configure React Router and initial page structure

---
+++ client/src/App.jsx
@@ -0,0 +1,55 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Services from "./pages/Services";
+import Packages from "./pages/Packages";
+import Booking from "./pages/Booking";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Public Layout */}
+        <Route element={<MainLayout />}>
+
+          <Route path="/" element={<Home />} />
+
+          <Route path="/about" element={<About />} />
+
+          <Route path="/portfolio" element={<Portfolio />} />
+
+          <Route path="/services" element={<Services />} />
+
+          <Route path="/packages" element={<Packages />} />
+
+          <Route path="/booking" element={<Booking />} />
+
+          <Route path="/contact" element={<Contact />} />
+
+        </Route>
+
+        {/* Standalone Pages */}
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* 404 */}
+
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
