79cd061611b133b0005b3e96af27a6ca00692fe4
Kashish Singh Jaiswal <kashishsinghjaiswal4@gmail.com>
2026-07-13 12:44:52 +0530

    Setup project architecture

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import "./styles/global.css";
 
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/navbar.css";
+import "./styles/footer.css";
+import "./styles/responsive.css";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
