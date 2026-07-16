94b7a489a5f242155e11edf9aa70ddbcc11b5bf4
Kashish Singh Jaiswal <kashishsinghjaiswal4@gmail.com>
2026-07-16 19:15:38 +0530

    Added card story details

---
+++ client/src/components/home/HeroSlider.jsx
@@ -0,0 +1,11 @@
+function HeroSlider() {
+  return (
+    <section style={{ height: "80vh", background: "#222", color: "#fff" }}>
+      <h1 style={{ padding: "100px", textAlign: "center" }}>
+        Hero Slider
+      </h1>
+    </section>
+  );
+}
+
+export default HeroSlider;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/home/Hero";
 import FeaturedGallery from "../components/home/FeaturedGallery";
+import HomeSlider from "../components/home/HeroSlider";
 
 function Home() {
   return (
     <>
       <Hero />
+      <HomeSlider />
       <FeaturedGallery />
     </>
   );
