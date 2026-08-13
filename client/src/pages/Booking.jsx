68f00bfd7efb60819437348b9c530a1ec1e56a28
Adirajak <adikazone@gmail.com>
2026-08-13 17:04:05 +0530

    changes in home

---
+++ client/src/pages/Booking.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Booking = () => {
+  return (
+    <div>
+      <h1>Booking Page</h1>
+      <p>This page is currently under construction.</p>
+    </div>
+  );
+};
+
+export default Booking;
--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,10 +1,14 @@
-function NotFound() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
   return (
-    <div className="container text-center mt-5">
-      <h1>404</h1>
-      <h3>Page Not Found</h3>
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you are looking for does not exist.</p>
+      <Link to="/">Go Back Home</Link>
     </div>
   );
-}
+};
 
 export default NotFound;
